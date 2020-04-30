2fa92ae22164d25993a7612f4cb83e29b3082de2
dqflying <dqflying@gmail.com>
2020-05-01 02:29:58 +0800

    init

---
+++ ch01/add.cpp
@@ -0,0 +1,25 @@
+/**
+ * @file add.cpp
+ * @brief 
+ * @author dqflying (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2020-05-01
+ * 
+ * @copyright Copyright (c) 2020  DQFLYING.INC
+ * 
+ * @par 新增文件:
+ * <table>
+ * <tr><th>Date            <th>Version      <th>Author       <th>Description
+ * <tr><td>2020-05-01      <td>1.0          <td>dqflying     <td>内容
+ * </table>
+ */
+#include <iostream>
+using namespace std;
+ 
+// main() 是程序开始执行的地方
+ 
+int main()
+{
+   cout << "Hello World"; // 输出 Hello World
+   return 0;
+}
