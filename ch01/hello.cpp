375eba73c8d994517d6578c4123d4f0513ca7d5e
dingqunfei <dqflying@gmail.com>
2020-05-02 03:19:31 +0800

    mul.cpp

---
+++ ch01/hello.cpp
@@ -0,0 +1,24 @@
+/**
+ * @file hello.cpp
+ * @brief 
+ * @author dqflying (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2020-05-01
+ * 
+ * @copyright Copyright (c) 2020  DQFLYING.INC
+ * 
+ * @par 新增文件:
+ * <table>
+ * <tr><th>Date            <th>Version      <th>Author       <th>Description
+ * <tr><td>2020-05-01      <td>1.0          <td>dqflying     <td>内容
+ * </table>
+ */
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char **argv)
+{
+    cout << "Hello, world!" << endl;
+    return 0;
+}
