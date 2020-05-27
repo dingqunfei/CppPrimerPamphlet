5932ef6540ef32f526e814fef1312107f6a8de95
dingqunfei <dqflying@gmail.com>
2020-05-28 01:32:14 +0800

    update

---
+++ ch02/test.cpp
@@ -0,0 +1,32 @@
+/**
+ * @file test.cpp
+ * @brief 
+ * @author dqflying (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2020-05-11
+ * 
+ * @copyright Copyright (c) 2020  DQFLYING.INC
+ * 
+ * @par 新增文件:
+ * <table>
+ * <tr><th>Date            <th>Version      <th>Author       <th>Description
+ * <tr><td>2020-05-11      <td>1.0          <td>dqflying     <td>内容
+ * </table>
+ */
+
+#include <iostream>
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+
+int main(int argc, char **argv)
+{
+    int *p = nullptr;
+    int *p1 = 0;
+    int *p2 = NULL;
+    //int &r3 = 50;
+    return 0;
+}
