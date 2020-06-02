15abbc9c00f61100d5051e85a64caac7fb133b1f
dingqunfei <dqflying@gmail.com>
2020-06-03 00:56:22 +0800

    add

---
+++ ch02/ex2_42.h
@@ -0,0 +1,25 @@
+/**
+ * @file ex2_42.h
+ * @brief 
+ * @author dqflying (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2020-06-03
+ * 
+ * @copyright Copyright (c) 2020  DQFLYING.INC
+ * 
+ * @par 新增文件:
+ * 
+ * 
+ * Date              Version        Author         LISENCE
+ * 2020-06-03        1.0            dqflying       XXXXXXX
+ * Description:
+ * 
+ * 
+ */
+
+#ifndef EX2_42_H
+#define EX2_42_H
+
+
+
+#endif
