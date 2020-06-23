a75b24dba9e4b1efd59e0d39fa4d7ee632e13b8d
dingqunfei <dqflying@gmail.com>
2020-06-24 01:01:59 +0800

    add

---
+++ ch03/ex3_31.cpp
@@ -0,0 +1,36 @@
+/**
+ * @file ex3_31.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2020-06-24
+ * 
+ * @copyright Copyright (c) 2020 DQFLYING.INC
+ * 
+ * @par:
+ * 
+ * 
+ * Date              Version        Author         LISENCE
+ * 2020-06-03        1.0            dqflying       XXXXXXX
+ * Description:
+ * 
+ * 
+ * 
+ */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    const int len = 100;
+    int arr[len];
+
+    for (size_t i = 0; i < len; ++i)
+    {
+        /* code */
+        arr[i] = i;
+    }
+    
+
+    return 0;
+}
