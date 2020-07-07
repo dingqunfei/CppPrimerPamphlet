75bc7fe98d407026d2aa8942c43b20d0100466f7
dingqunfei <dqflying@gmail.com>
2020-07-08 00:39:07 +0800

    add

--- ch03/ex3_43.cpp
+++ ch03/ex3_43.cpp
@@ -1,11 +1,23 @@
 /**
  * @file ex3_43.cpp
- * @author your name (you@domain.com)
  * @brief 
- * @version 0.1
- * @date 2020-07-01
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2020-07-02
  * 
- * @copyright Copyright (c) 2020
+ * @copyright Copyright (c) 2020 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2020-07-02
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
  * 
  */
 
@@ -14,7 +26,29 @@
 
 int main(int argc, char **argv)
 {
-    
+    int arr[3][4] = 
+    {
+        {0, 1, 2, 3},
+        {4, 5, 6, 7},
+        {8, 9, 10, 11}
+    };
+
+    for(const int(&row)[4]: arr)
+    {
+        for(int col: row)
+        {
+            std::cout << col << std::endl;
+        }
+    }
+
+    // for loop
+    for (size_t i = 0; i != 3; ++i)
+        for (size_t j = 0; j != 4; ++j) std::cout << arr[i][j] << std::endl;
+
+    // using pointers.
+    for (int(*row)[4] = arr; row != arr + 3; ++row)
+        for (int *col = *row; col != *row + 4; ++col) std::cout << *col << std::endl;;
+
 
     return 0;
 }
---
+++ ch04/test.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file test.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2020-07-05
+ * 
+ * @copyright Copyright (c) 2020 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2020-07-05
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+    bool b = true;
+    int a = b;
+    bool br = -b;
+
+
+    int *pi;
+    pi = 0;
+
+    return 0;
+}
