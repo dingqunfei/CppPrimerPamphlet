f12622507234caea71c257c4e3aa415e0756f176
dingqunfei <dqflying@gmail.com>
2020-07-15 23:45:13 +0800

    add

--- ch06/ex6_17.cpp
+++ ch06/ex6_17.cpp
@@ -26,12 +26,32 @@
 
 bool any_capital(const std::string &str)
 {
-    
+    for (auto ch : str)
+    {
+        /* code */
+        if (isupper(ch))
+        {
+            /* code */
+            return true;
+        }
+    }
+    return false;
+}
 
+void to_lowercase(std::string &str)
+{
+    for(auto &ch : str)
+    {
+        ch = toupper(ch);
+    }
 }
 
 int main(int argc, char **argv)
 {
-
+    std::string str = "abcABCxyzXYZ";
+    bool is_cap = any_capital(str);
+    to_lowercase(str);
+    std::cout << is_cap << std::endl;
+    std::cout << str << std::endl;
     return 0;
 }
