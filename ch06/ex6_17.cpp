df50700b4898152ff2e46f59edd12d9b47dcc61a
dingqunfei <dqflying@gmail.com>
2020-07-15 23:34:04 +0800

    add

---
+++ ch06/ex6_17.cpp
@@ -0,0 +1,37 @@
+/**
+ * @file ex6_17.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2020-07-15
+ * 
+ * @copyright Copyright (c) 2020 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2020-07-15
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+#include <string>
+#include <iostream>
+
+bool any_capital(const std::string &str)
+{
+    
+
+}
+
+int main(int argc, char **argv)
+{
+
+    return 0;
+}
