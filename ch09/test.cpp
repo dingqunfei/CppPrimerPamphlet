8d1d212e4b2d885f4256052c2cb3bc1057705606
dingqunfei <dqflying@gmail.com>
2020-08-02 23:08:17 +0800

    add

---
+++ ch09/test.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file test.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2020-08-02
+ * 
+ * @copyright Copyright (c) 2020 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2020-08-02
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+#include <string>
+#include <vector>
+
+int main(int argc, char **argv)
+{
+    std::string str = "abc";
+    char *ch = "abc";
+
+    return 0;
+}
