7b6f1b4d68eac5d93d163001e13665ba41a64237
dingqunfei <dqflying@gmail.com>
2020-08-17 23:06:48 +0800

    add

---
+++ ch11/test.cpp
@@ -0,0 +1,35 @@
+/**
+ * @file test.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2020-08-16
+ * 
+ * @copyright Copyright (c) 2020 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2020-08-16
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+#include <utility>
+#include <string>
+
+std::pair<std::string, std::string> name;
+
+int main()
+{
+    name.first = "ding";
+    name.second = "qunfei";
+
+    return 0;
+}
