ec07e168acf15a51831baa415f5e4d16c2d1226d
dingqunfei <dqflying@gmail.com>
2020-08-19 21:19:33 +0800

    add

--- ch11/test.cpp
+++ ch11/test.cpp
@@ -22,7 +22,12 @@
  */
 
 #include <utility>
+#include <hash_map>
 #include <string>
+
+#include <map>
+#include <vector>
+#include <set>
 
 std::pair<std::string, std::string> name;
 
@@ -31,5 +36,15 @@ int main()
     name.first = "ding";
     name.second = "qunfei";
 
+    std::vector<int> intVec = {2, 4, 6, 8, 2, 4, 6, 8};
+    std::set<int> intSet(intVec.cbegin(), intVec.cend());
+    intSet.insert({1, 3, 5, 7, 1, 3, 5, 7});
+
+    std::map<std::string, int> wordCount;
+    wordCount.insert({"ding", 1});
+    wordCount.insert(std::make_pair("qun", 1));
+    wordCount.insert(std::pair<std::string, int>("fei", 1));
+
+    auto res = std::hash<std::string>()("dingqunfei");
     return 0;
 }
---
+++ ch12/test.cpp
@@ -0,0 +1,33 @@
+/**
+ * @file test.cpp
+ * @brief 
+ * @author dingqunfei (dqflying@gmail.com)
+ * @version 1.0
+ * @date 2020-08-19
+ * 
+ * @copyright Copyright (c) 2020 DQFLYING
+ * 
+ * @par :
+ * 
+ * 
+ * Date        : 2020-08-19
+ * Version     : 1.0
+ * Author      : dqflying
+ * Lisence     : 
+ * Description : 
+ * 
+ * 
+ * 
+ * 
+ */
+
+#include <memory>
+#include <string>
+#include <vector>
+
+int main(int argc, char **argv)
+{
+    
+
+    return 0;
+}
