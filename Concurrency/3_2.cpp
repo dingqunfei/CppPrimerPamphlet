fee63cd05d0e3c6c01c42c08376797c993ffafde
dingqunfei <dqflying@gmail.com>
2021-06-22 01:37:10 +0800

    add

--- Concurrency/3_2.cpp
+++ Concurrency/3_2.cpp
@@ -20,13 +20,31 @@ long f(long x)
 
 int main()
 {   
-    int x = 16;
-    int count = 0;
-    while (x + 1)
-    {
-        count++;
-        x |= (x + 1);
-    }
-    std::cout << count << std::endl;
-    return count;
+        string s = "aaaa";
+        int size = s.size();
+
+        vector<vector<int>> dp(size, vector<int>(size, 0));
+        for(int n = 0; n < size; ++n)
+        {
+            dp[n][n] = 1;
+        }
+        int max = 1;
+        int left = 0, right = 0;
+        for(int i = size - 1; i >= 0; --i)
+        {
+            for(int j = i+1; j < size; ++j)
+            {
+                if((s[i] == s[j]) && ((i+1 == j-1) || (dp[i+1][j-1] != 0)))
+                {
+                    dp[i][j] = dp[i+1][j-1]+2;
+                    if(dp[i][j] > max)
+                    {
+                        max = dp[i][j];
+                        left = i;
+                        right = j;
+                    }
+                }
+            }
+        }
+        return 0;
 }
